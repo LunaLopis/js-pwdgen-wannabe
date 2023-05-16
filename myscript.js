6c6bbfe182dce0765dfe4ce88b4b1c703e1565e1
Luna Lopis <lunalopis59@gmail.com>
2023-05-16 23:40:35 +0200

    esercizio completato

--- myscript.js
+++ myscript.js
@@ -1,4 +1,17 @@
+let name = prompt ('come ti chiami?');
 
+document.getElementById('greetings').innerHTML = name;
+
+
+let surname = prompt ('qual è il tuo cognome?');
+
+document.getElementById('greetings').innerHTML = surname;
+
+let color = prompt ('qual è il tuo colore preferito?');
+
+document.getElementById('greetings').innerHTML = color;
+
+document.getElementById('greetings').innerHTML = `${name}${surname}${color}21` ;
 
 
 
